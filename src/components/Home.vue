5075ab68d9e661d2ac8a9529aed5f1960e6e8392
Will <66789715+liwll@users.noreply.github.com>
2021-09-18 14:43:40 -0400

    Add projects section and build for production

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="home">
-    <div class="sidebar">
-      <div class="about">
-        <div id="name"><span id="name-text">Will Li</span></div>
-        <div id="bio">
-          Hello! I am a student at Stony Brook University with a passion for Computer Science. 
-          I love building different websites and apps, 
-          and I am also super interested in researching deep learning and computer vision.
-          My hobbies include learning new languages & skills, making music, and reading books. 
-        </div>
-        <div id="history">
-          <div class="history-item">Currently: Undergraduate Researcher @ Stony Brook University</div>
-          <div class="history-item">Previously: Software Engineer Intern @ Optum</div>
-        </div>
-        <div id="icons">
-          <a href="https://github.com/liwll"><i class="bi bi-github"></i></a>
-          <a href="https://www.linkedin.com/in/liwll/"><i class="bi bi-linkedin"></i></a>
-          <a href="mailto:will.li@stonybrook.edu"><i class="bi bi-envelope-fill"></i></a>
-          <a href="https://drive.google.com/file/d/1RGFoJ43YIma4estjR4f6dk2g5nyS_1LS/view?usp=sharing"><i class="bi bi-file-earmark-text-fill"></i></a>
-        </div>
-        <div id="quote">
-          <span id="quote-text">"Man's reach exceeds his imagination!" - Robert Angier, <em>The Prestige</em></span>
-        </div>
+    <div class="about">
+      <div id="name"><span id="name-text">Will Li</span></div>
+      <div id="bio">
+        Hello! I am a student at Stony Brook University with a passion for Computer Science. 
+        I love building different websites and apps, 
+        and I am also super interested in researching deep learning and computer vision.
+        My hobbies include learning new languages & skills, making music, and reading books. 
+      </div>
+      <div id="history">
+        <div class="history-item">Currently: Undergraduate Researcher @ Stony Brook University</div>
+        <div class="history-item">Previously: Software Engineer Intern @ Optum</div>
+      </div>
+      <div id="icons">
+        <a href="https://github.com/liwll"><i class="bi bi-github"></i></a>
+        <a href="https://www.linkedin.com/in/liwll/"><i class="bi bi-linkedin"></i></a>
+        <a href="mailto:will.li@stonybrook.edu"><i class="bi bi-envelope-fill"></i></a>
+        <a href="https://drive.google.com/file/d/1RGFoJ43YIma4estjR4f6dk2g5nyS_1LS/view?usp=sharing"><i class="bi bi-file-earmark-text-fill"></i></a>
+      </div>
+      <div id="quote">
+        <span id="quote-text">"Man's reach exceeds his imagination!" - Robert Angier, <em>The Prestige</em></span>
       </div>
     </div>
+    <Projects/>
   </div>
 </template>
 
 <script>
+import Projects from './Projects.vue'
+
 export default {
   name: 'Home',
+  components: {
+    Projects
+  },
   props: {
     msg: String
   },
@@ -43,11 +47,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.sidebar {
-  margin: auto;
-}
 .about {
-  margin: 200px 100px;
+  margin: 100px auto;
+  width: 50%;
+
 }
 #name {
   font-size: 3em;
@@ -83,25 +86,5 @@ i:hover {
   padding: 9px;
   border: 1px solid #ED960B;
   border-radius: 5px;
-}
-.content {
-  position: fixed;
-  right: 0;
-  height: 100vh;
-  width: 63vw;
-}
-.projects {
-  width: 90%;
-  margin: 12vh auto;
-  display: flex;
-  flex-wrap: wrap;
-}
-.project-card {
-  flex: 0 0 calc(50% - 33px);
-  height: 200px;
-  border: 1px solid #ED960B;
-  border-radius: 5px;
-  padding: 10px;
-  margin: 0.5%;
 }
 </style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="projects">
+        <h1>Projects</h1>
+        <div class="project" v-for="project in projects" :key="project.id">
+            <a class="name" :href=project.link target="_blank" rel="noopener noreferrer">
+                {{ project.name }} <i class="bi bi-link-45deg"></i>
+            </a>
+            <div class="description">
+                {{ project.description }}
+            </div>
+        </div>
+    </div>
+    
+  
+  
+</template>
+
+<script>
+export default {
+    name: 'Projects',
+    data() {
+        return {
+            projects: [
+                {
+                    id: 0,
+                    name: 'Covid Simulation',
+                    description: 'A web app featuring several visualizations and an interactive simulation which demonstrates how to "flatten the curve" through vaccinations and social distancing.',
+                    link: 'https://github.com/liwll/covid-sim-vizathon-2021',
+                },
+                {
+                    id: 1,
+                    name: 'Todo List',
+                    description: 'Organize your tasks with todo lists in this app.',
+                    link: 'https://github.com/liwll/todo-list',
+                },
+                {
+                    id: 2,
+                    name: 'Reaction Timer Game',
+                    description: 'A simple web app which measures your visual reaction time.',
+                    link: 'https://github.com/liwll/reaction-timer-game',
+                },
+                {
+                    id: 3,
+                    name: 'Restaraunt Page',
+                    description: 'A restaurant page template with a home, menu, and about page.',
+                    link: 'https://github.com/liwll/restaurant-page',
+                },
+                {
+                    id: 4,
+                    name: 'Tic-Tac-Toe',
+                    description: 'A simple Tic-Tac-Toe game for two players.',
+                    link: 'https://github.com/liwll/tic-tac-toe',
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 2em;
+}
+.projects {
+    display: block;
+    margin: 10px auto;
+    width: 50%;
+}
+.project {
+    margin: 20px 0;
+    border: 1px solid white;
+    border-radius: 10px;
+    padding: 15px;
+}
+.name {
+    font-size: 1.7em;
+    cursor: pointer;
+}
+a:visited, a:link {
+  color: inherit;
+  text-decoration: none;
+}
+.description {
+    margin-top: 15px;
+    word-wrap: break-word;
+}
+</style>
